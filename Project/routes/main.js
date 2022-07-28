0f3c5a8fe3e8f0ceb2fa1ceafe96fd272f257478
zaeemsuhartono <zaeemsuhartono@gmail.com>
2022-07-28 23:46:48 +0800

    Refactor

--- Project/routes/main.js
+++ Project/routes/main.js
@@ -3,27 +3,34 @@ module.exports = function (app) {
     var fs = require('fs');
 
     var firebaseRef = db.ref("main");
-
     var usersRef = firebaseRef.child("users");
+    var journalRef = firebaseRef.child("journal");
 
     app.get("/", function (req, res) {
       
-      // Example
+      // Example write
       /*
       usersRef.set({
-        john1: {
-          "id" : 1,
-          "username" : "John",
+        0: {
+          "username" : "john1",
           "password" : "asdf23radvv",
+          "full_name" : "John Malkovich",
           "email" : "test123@gmail.com"
         }
       });
-      **/
 
+      journalRef.set({
+        0:{
+          "userID" : 0,
+          "date_added" : Date(),
+          "content" : "Hello Journal"
+        }
+      });
+      **/
       res.render("homepage.html");
     });
 
-    app.get("/mood-tracker", function (req, res) {
+    app.get("/mood_tracker", function (req, res) {
 
       var user_data, mood_tracker_data;
 
@@ -33,6 +40,29 @@ module.exports = function (app) {
       console.log(user_data);
       console.log(mood_tracker_data);
 
-      res.render("mood-tracker.html", {moodTracker : mood_tracker_data})
+      res.render("mood_tracker.html", {moodTracker : mood_tracker_data})
     });
+
+    app.get("/journal", function (req, res) {
+
+      // Example retrieve data
+      /*
+      usersRef.child(0).get().then((snapshot) => {
+        if (snapshot.exists()) {
+          console.log(snapshot.val());
+        } else {
+          console.log("No data available");
+        }
+      }).catch((error) => {
+        console.error(error);
+      });
+      **/
+
+      // Get users journal entries
+      //journalRef.orderBy("userID")
+
+      res.render("journal.html");
+    });
+
+    //app.post()
 }
