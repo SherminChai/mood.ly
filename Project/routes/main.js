fd6a72b06f7e75ccb85eb35f21e93ae13a5a2a82
Jiaying1216 <64124037+Jiaying1216@users.noreply.github.com>
2022-08-11 14:24:05 +0800

    Update main.js

--- Project/routes/main.js
+++ Project/routes/main.js
@@ -1,8 +1,12 @@
 module.exports = function (app) {
 
+  var fs = require('fs');
+  var admin = require('firebase-admin');
   var firebaseRef = db.ref("main");
   var usersRef = firebaseRef.child("users");
   var journalRef = firebaseRef.child("journal");
+  var signUpRef = firebaseRef.child("sign-up");
+  var signInRef = firebaseRef.child("sign-in");
 
   var DateUtil = require("./util/dateUtil.js");
   var dateUtil = new DateUtil();
@@ -125,4 +129,136 @@ module.exports = function (app) {
       res.render("homepage.html");
     });
   });
+  
+  app.get("/signIn", function (req, res) {
+  res.render('signIn.html');
+});
+  
+//   app.post("/sign_in", function (req, res) {
+
+//     admin
+//     .auth()
+//     .signInWithEmailAndPassword({
+//      "email" : req.body.email,
+//      "password" : req.body.password
+//     })
+//     .then(function () {
+//       admin.auth().currentUser.getIdToken(true).then(function(idToken){
+//              res.send(idToken)
+//              res.end()
+//           }).catch(function (error) {
+//               //Handle error
+//           });
+//  }).catch(function (error) {
+//        //Handle error
+//  });
+//     // .then(({ user }) => {
+//     //   console.log(user);
+//     //   return user.getIdToken()
+//     // })
+//     res.render('profile.html');
+
+//   });
+
+// app.post('/sign_in', async(req, res) => {
+//   const {email, password} = req.body;
+//   admin
+//   .auth()
+//   .signInWithEmailAndPassword(email, password)
+//   .then((userCredential) => {
+//   var user = userCredential.user;
+//   })
+//   .catch((error) => {
+//   var errorCode = error.code;
+//   var errorMessage = error.message;
+//   });
+//   res.render('profile.html');
+//   })
+
+//   app.get("/signUp", function (req, res) {
+//     res.render("signUp.html");
+//   });
+
+//saving sign-up to database
+
+app.post('/sign_up', function (req, res) {
+
+  admin
+      .auth()
+      .createUser({
+        "email" : req.body.email,
+        "password" : req.body.password
+      })
+      .then((userData) => {
+        signUpRef.push().set({
+          "userID": userData.uid,
+          "email" : req.body.email,
+          "password" : req.body.password,
+          "username" : req.body.user_name,
+          "age" : req.body.age,
+          "gender" : req.body.gender,
+          "phone_number" : req.body.phone_number,
+          "educational_level" : req.body.educational_level,
+          "school" : req.body.school
+
+      });
+    })
+    // .then(({ user }) => {
+    //   console.log(user);
+    //   return user.getIdToken()
+    // })
+    .catch((error) => {
+        console.log('Error creating userr:',error);
+    });
+    res.render("signIn.html");
+});
+
+app.get("/profile", function (req, res) {
+  res.render("profile.html");
+})
+
+app.get("/profile", function (req, res) {
+  const sessionCookie = req.cookies.session || "";
+
+  admin
+    .auth()
+    .verifySessionCookie(sessionCookie, true /** checkRevoked */)
+    .then((userData) => {
+      console.log("Logged in:", userData.email)
+      res.render("profile.html");
+    })
+    .catch((error) => {
+      res.redirect("/signIn");
+    });
+});
+
+app.get("/", function (req, res) {
+  // res.clearCookie("__session");
+  res.render("index.html");
+});
+
+app.post("/sessionLogin", (req, res) => {
+  const idToken = req.body.idToken.toString();
+
+  const expiresIn = 60 * 60 * 24 * 5 * 1000;
+
+  admin
+    .auth()
+    .createSessionCookie(idToken, { expiresIn })
+    .then((sessionCookie) => {
+        const options = { maxAge: expiresIn, httpOnly: true };
+        res.cookie("session", sessionCookie, options);
+        res.end(JSON.stringify({ status: "success" }));
+      },
+      (error) => {
+        res.status(401).send("UNAUTHORIZED REQUEST!");
+      }
+    );
+});
+
+app.get("/sessionLogout", (req, res) => {
+  res.clearCookie("session");
+  res.redirect("/signIn");
+});
+
 }
